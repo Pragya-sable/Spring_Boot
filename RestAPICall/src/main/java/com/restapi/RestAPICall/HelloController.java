b715afeb15b6977927c629bbd8d5d5770b7d1901
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-02-21 22:52:04 +0530

    Make REST Call to show Hello Mark from BridgeLabz

--- RestAPICall/src/main/java/com/restapi/RestAPICall/HelloController.java
+++ RestAPICall/src/main/java/com/restapi/RestAPICall/HelloController.java
@@ -1,6 +1,7 @@
 package com.restapi.RestAPICall;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,12 @@ public class HelloController {
     public String sayHello(){
         return "Hello from Bridgelabz";
     }
+
+    @GetMapping("/hello/query")
+    public String greetUser(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+
 
 }
